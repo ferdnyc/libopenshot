9e70dbf3847c7fb5edcf86b7e54ecbeb769ef93e
FeRD (Frank Dana) <ferdnyc@gmail.com>
2020-04-16 19:10:39 -0400

    Re-fix install paths

--- appveyor/build-libopenshot-audio.bat
+++ appveyor/build-libopenshot-audio.bat
@@ -2,7 +2,7 @@
 git clone https://github.com/OpenShot/libopenshot-audio
 mkdir libopenshot-audio-build-%PLAT_ARCH%
 cd libopenshot-audio-build-%PLAT_ARCH%
-cmake -DCMAKE_INSTALL_PREFIX=..\install-cache\%PLAT_ARCH% -DCMAKE_BUILD_TYPE=Debug -G "MinGW Makefiles" ..\libopenshot-audio
+cmake -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%\install-%PLAT_ARCH% -DCMAKE_BUILD_TYPE=Debug -G "MinGW Makefiles" ..\libopenshot-audio
 cmake --build . -- VERBOSE=1
 cmake --build . --target install
 cd ..
--- appveyor/build-unittest-cpp.bat
+++ appveyor/build-unittest-cpp.bat
@@ -2,7 +2,7 @@
 git clone https://github.com/unittest-cpp/unittest-cpp
 mkdir unittest-cpp-build
 cd unittest-cpp-build
-make -DCMAKE_INSTALL_PREFIX=..\install-cache\%PLAT_ARCH% -G "MinGW Makefiles" ..\unittest-cpp
+make -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%\install-%PLAT_ARCH% -G "MinGW Makefiles" ..\unittest-cpp
 cmake --build . -- VERBOSE=1
 cmake --build . --target install
 cd ..
