0b54724807397ba205f2e98e6654fba69249f8a4
FeRD (Frank Dana) <ferdnyc@gmail.com>
2020-09-19 17:32:17 -0400

    AppVeyor: Remove MacOS building

--- appveyor/build-libopenshot-audio.bat
+++ appveyor/build-libopenshot-audio.bat
@@ -1,8 +1,6 @@
 @echo on
 git clone https://github.com/OpenShot/libopenshot-audio
-mkdir libopenshot-audio-build-%PLAT_ARCH%
-cd libopenshot-audio-build-%PLAT_ARCH%
-cmake -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%\install-%PLAT_ARCH% -DCMAKE_BUILD_TYPE=Debug -G "MinGW Makefiles" ..\libopenshot-audio
-cmake --build . -- VERBOSE=1
-cmake --build . --target install
-cd ..
+set BUILD_DIR=libopenshot-audio-build-%PLAT_ARCH%
+cmake -B %BUILD_DIR% -S libopenshot-audio -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%\install-%PLAT_ARCH% -DCMAKE_BUILD_TYPE=Debug -G "MinGW Makefiles"
+cmake --build %BUILD_DIR% --verbose
+cmake --install %BUILD_DIR%
